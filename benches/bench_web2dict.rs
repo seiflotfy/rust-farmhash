35a57e802a5897ffe23fa286d8a00bfbb69530a1
Seif Lotfy <seif.lotfy@gmail.com>
2015-07-15 19:01:32 +0200

    Add proper benchmarks

--- benches/bench_web2dict.rs
+++ benches/bench_web2dict.rs
@@ -1,56 +1,108 @@
-/*
-mod tests {
-    #![feature(test)]
-    extern crate test;
-    extern crate time;
+#![feature(test)]
+extern crate test;
+extern crate farmhash;
+extern crate fnv;
 
-    use super::*;
-    use self::test::Bencher;
-    use std::fs::File;
-    use std::io::prelude::*;
-    use std::path::Path;
-    use self::time::precise_time_s;
-    use std::error::Error;
+use self::farmhash::*;
+use self::test::Bencher;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::error::Error;
+use std::hash::{Hash, SipHasher, Hasher};
 
-    #[bench]
-    fn bench_web2_dict(b: &mut Bencher) {
-        let path = Path::new("/usr/share/dict/web2");
-        let display = path.display();
-     
-        // Open the path in read-only mode, returns `io::Result<File>`
-        let mut file = match File::open(&path) {
-            // The `description` method of `io::Error` returns a string that
-            // describes the error
-            Err(why) => panic!("couldn't open {}: {}", display,
-                                                       Error::description(&why)),
-            Ok(file) => file,
-        };
 
-        let mut web2 = String::new();
-        match file.read_to_string(&mut web2) {
-            Err(why) => panic!("couldn't read {}: {}", display,
-                                                       Error::description(&why)),
-            Ok(_) => {},
-        }
+#[bench]
+fn bench_sip_web2_dict(b: &mut Bencher) {
+    let path = Path::new("/usr/share/dict/web2");
+    let display = path.display();
 
-        {
-            let split: Vec<&str> = web2.split("\n").collect();
-            let split_len = split.len();
-            let mut v: Vec<u64> = vec![];
-            let mut needed_time: f64 = 0.0;
-     
-            for s in split {
-                let starttime = precise_time_s();
-                let res = farmhash::hash64(&s.as_bytes());
-                needed_time += precise_time_s() - starttime;
-                v.push(res);
-            }
-            v.sort();
-            v.dedup();
-            let dedup_percent = split_len - v.len();
-            println!("farmhash required {} s with {}/{} collisions", needed_time, dedup_percent, split_len);
-        }
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(file) => file,
+    };
 
+
+    let mut web2 = String::new();
+    match file.read_to_string(&mut web2) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(_) => {},
     }
+
+    b.iter(|| {
+        let mut hasher = SipHasher::default();
+        let split: Vec<&str> = web2.split("\n").collect();
+        for s in split {
+            s.hash(&mut hasher);
+            hasher.finish();
+        }
+    });
 }
-*/
+
+#[bench]
+fn bench_fnv_web2_dict(b: &mut Bencher) {
+    let path = Path::new("/usr/share/dict/web2");
+    let display = path.display();
+ 
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(file) => file,
+    };
+
+
+    let mut web2 = String::new();
+    match file.read_to_string(&mut web2) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(_) => {},
+    }
+
+    b.iter(|| {
+        let mut hasher = fnv::FnvHasher::default();
+        let split: Vec<&str> = web2.split("\n").collect();
+        for s in split {
+            s.hash(&mut hasher);
+            hasher.finish();
+        }
+    });
+}
+
+#[bench]
+fn bench_farm_web2_dict(b: &mut Bencher) {
+    let path = Path::new("/usr/share/dict/web2");
+    let display = path.display();
+ 
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    let mut web2 = String::new();
+    match file.read_to_string(&mut web2) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                                                   Error::description(&why)),
+        Ok(_) => {},
+    }
+
+    b.iter(|| {
+        let mut hasher = farmhash::FarmHasher::default();
+        let split: Vec<&str> = web2.split("\n").collect();
+        for s in split {
+            s.hash(&mut hasher);
+            hasher.finish();
+        }
+    });
+}
