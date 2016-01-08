d03bbb8a620ea9242f775330537d47048cb51195
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2016-01-08 08:07:13 -0500

    Document caveats from C++ implementation

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,8 @@ use farmhashxo::xo_hash64_with_seed;
 use farmhashxo::xo_hash64_with_seeds;
 use std::hash::Hasher;
 
-/// Create a new farmhash based u32 for an array of bytes.
+/// Create a new farmhash based u32 for an array of bytes.  Hash value may vary
+/// with library version and platform.
 ///
 /// # Examples
 ///
@@ -27,12 +28,15 @@ use std::hash::Hasher;
 pub fn hash32(s: &[u8]) -> u32 {
     mk_hash32(s)
 }
+
 /// Create a new farmhash based u32 for an array of bytes with a given seed.
+/// Hash value may vary with library version and platform.
 pub fn hash32_with_seed(s: &[u8], seed: u32) -> u32 {
     mk_hash32_with_seed(s, seed)
 }
 
-/// Create a new farmhash based u64 for an array of bytes.
+/// Create a new farmhash based u64 for an array of bytes.  Hash value may
+/// vary with library version and platform.
 ///
 /// # Examples
 ///
@@ -46,16 +50,19 @@ pub fn hash64(s: &[u8]) -> u64 {
 }
 
 /// Create a new farmhash based u64 for an array of bytes with a given seed.
+/// Hash value may vary with library version and platform.
 pub fn hash64_with_seed(s: &[u8], seed: u64) -> u64 {
     xo_hash64_with_seed(s, seed)
 }
 
 /// Create a new farmhash based u64 for an array of bytes with 2 given seeds.
+/// Hash value may vary with library version and platform.
 pub fn hash64_with_seeds(s: &[u8], seed0: u64, seed1: u64) -> u64 {
     xo_hash64_with_seeds(s, seed0, seed1)
 }
 
-/// Create a stable farmhash based u32 for an array of bytes.
+/// Create a new farmhash based u32 for an array of bytes.  Fingerprint value
+/// should be portable and stable across library versions and platforms.
 ///
 /// # Examples
 ///
@@ -68,7 +75,8 @@ pub fn fingerprint32(s: &[u8]) -> u32 {
     mk_hash32(s)
 }
 
-/// Create a stable farmhash based u64 for an array of bytes.
+/// Create a stable farmhash based u64 for an array of bytes.  Fingerprint value
+/// should be portable and stable across library versions and platforms.
 ///
 /// # Examples
 ///
